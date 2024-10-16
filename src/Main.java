412aa866b70cb3fdfdf28a562cb655081fb6da8c
Victor <l24121359.morelia.tecnm.mx>
2024-10-16 00:07:13 -0600

    Tarea 3

---
+++ src/Main.java
@@ -0,0 +1,180 @@
+public class Main {
+    public static void main(String[] args) {
+
+      //TAREA 3
+
+        //UNO
+
+    boolean operacionUno = 3 < 5 && 7 >= 2;
+
+    if (operacionUno) {
+        System.out.println("Operacion uno verdadera");
+
+    }else {
+        System.out.println("Operacion uno falsa");
+    }
+
+    //DOS
+    boolean operacionDos = 10 != 8 || 15== 15;
+
+    if (operacionDos) {
+        System.out.println("Operacion dos verdadera");
+
+    }else {
+        System.out.println("Operacion dos falsa");
+    }
+
+    //TRES
+    boolean operacionTres = !(5 > 6) && 2 <= 3;
+
+    if (operacionTres) {
+        System.out.println("Operacion tres verdadera");
+
+    }else {
+        System.out.println("Operacion tres falsa");
+    }
+
+    //CUATRO
+    boolean opreacionCuatro = (8 == 8 || 3 < 1) && 6 > 2;
+
+    if (opreacionCuatro) {
+        System.out.println("Operacion cuatro verdadera");
+    }else {
+        System.out.println("Operacion cuatro falsa");
+    }
+
+    //CINCO
+    boolean operacionCinco = 12 != 10 && (4 < 8 || 9 == 9);
+
+    if (operacionCinco) {
+        System.out.println("Operacion cinco verdadera");
+    }else {
+        System.out.println("Operacion cinco falsa");
+    }
+
+    //SEIS
+    boolean operacionSeis = (3 + 5) == 8 || (7 * 2) <= 14;
+
+    if (operacionSeis) {
+        System.out.println("Operacion seis verdadera");
+    }else {
+        System.out.println("Operacion seis falsa");
+    }
+
+    //SIETE
+    boolean operacionSiete = !(9 <= 5) && (4 == 4 || 6 != 6);
+    if (operacionSiete) {
+        System.out.println("Operacion siete verdadera");
+    }else {
+        System.out.println("Operacion siete falsa");
+    }
+
+    //OCHO
+    boolean operacionOcho = 10 % 2 == 0 && (5 > 1 || 12 != 10);
+    if (operacionOcho) {
+        System.out.println("Operacion ocho verdadera");
+    }else{
+        System.out.println("Operacion ocho falsa");
+    }
+
+    //NUEVE
+    boolean operacionNueve = (3 * 3 == 9) && !(8 < 5 || 7>= 9);
+    if (operacionNueve) {
+        System.out.println("Operacion nueve verdadera");
+    }else {
+        System.out.println("Operacion nueve falsa");
+    }
+
+    //DIEZ
+    boolean operacionDiez = !(5 + 5 != 10) && (2 * 3 > 5 || 7 >= 9);
+    if (operacionDiez) {
+        System.out.println("Operacion diez verdadera");
+    }else {
+        System.out.println("Operacion diez falsa");
+    }
+
+    //ONCE
+    boolean operacionOnce = 20 / 4 == 5 || !(10 % 3 == 1 && 4 >= 3);
+    if (operacionOnce) {
+        System.out.println("Operacion once verdadera");
+    }else {
+        System.out.println("Operacion once falsa");
+    }
+
+    //DOCE
+    boolean operacionDoce = !(7 > 3 && 2 == 2) || (5 * 2 < 12 && 8 >= 8);
+    if (operacionDoce) {
+        System.out.println("Operacion doce verdadera");
+    }else {
+        System.out.println("Operacion doce falsa");
+    }
+
+    //TRECE
+    boolean operacionTrece = (15 - 5 > 8 ) && (3 == 3 || 7 < 1 ) && 12 >= 10;
+    if (operacionTrece) {
+        System.out.println("Operacion trece verdadera");
+    }else {
+        System.out.println("Operacion trece falsa");
+    }
+
+    //CATORCE
+    boolean operacionCatorce = 9 % 3 == 0 && (8 <= 12 || 5 + 5 != 10) && !(7 > 8);
+    if (operacionCatorce) {
+        System.out.println("Operacion catorce verdadera");
+    }else{
+        System.out.println("Operacion catorce falsa");
+    }
+
+    //QUINCE
+    boolean operacionQuince = !(10 != 10) && (5 * 3 == 15 || 2 < 1) && 9 >= 9;
+    if (operacionQuince) {
+        System.out.println("Operacion quince verdadera");
+    }else{
+        System.out.println("Operacion quince falsa");
+    }
+
+    //DIECISEIS
+    boolean operacionDieciseis = (4 + 6 == 10 || 2 == 3) && !(8 < 8 && 3 > 2) || 7 % 2 == 1;
+    if (operacionDieciseis) {
+        System.out.println("Operacion dieciseis verdadera");
+    }else {
+        System.out.println("Operacion dieciseis falsa");
+    }
+
+    //DIECISIETE
+    boolean operacionDiecisiete = (12 / 3 > 4) && (5 == 5 || 10 != 10) && !(8 >= 9);
+    if (operacionDiecisiete) {
+        System.out.println("Operacion diecisiete verdadera");
+    }else{
+        System.out.println("Operacion diecisiete falsa");
+    }
+
+    //DIECIOCHO
+    boolean operacionDieciocho = !(7 + 3 < 10 ) && (9 / 3 == 3 || 6 < 2) && (5 * 4 >= 20);
+    if (operacionDieciocho) {
+        System.out.println("Operacion dieciocho verdadera");
+    }else {
+        System.out.println("Operacion dieciocho falsa");
+    }
+
+    //DIECINUEVE
+    boolean operacionDiecinueve = ((10 - 2) != 8) && !(6 >= 7 || 2 == 2) && (12 % 3 == 0);
+    if (operacionDiecinueve) {
+        System.out.println("Operacion diecinueve verdadera");
+    }else {
+        System.out.println("Operacion diecinueve falsa");
+    }
+
+    //VEINTE
+    boolean operacionVeinte = !(15 > 5 && 3 + 2 == 5) || (9 < 10 && !(6 <= 4)) && (7 * 2 != 14);
+    if (operacionVeinte) {
+        System.out.println("Operacion veinte verdadera");
+    }else {
+        System.out.println("Operacion veinte falsa");
+    }
+
+
+
+
+    }
+}
