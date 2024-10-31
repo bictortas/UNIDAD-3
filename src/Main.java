585a54467f22621c7bb925043c337a30b56cb30f
Victor <l24121359.morelia.tecnm.mx>
2024-10-30 23:01:55 -0600

    Tarea 9

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,61 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-    //TAREA 4
+        String cadena = "book";
+        int largo = Integer.valueOf(cadena.length());
+        //MITADES
+        String mitadUno = cadena.substring(0, largo/2);
+        String mitadDos = cadena.substring(largo/2);
+        //CONTADORES
+        int contarVocalesA = 0;
+        int contarVocalesB = 0;
+        int a = 0;
+        int b = 0;
+        //VOCALES
+        String vocales = "aeiouAEIU";
 
-        //NUMERO DIVISIBLE ENTRE 3, 5 O AMBOS
-    Scanner intput = new Scanner(System.in);
-        System.out.println("Ingresa un numero: ");
-        int numeroUno = intput.nextInt();
-
-        if (numeroUno % 3 == 0 && numeroUno % 5 == 0) {
-            System.out.println("El numero es divisible entre 3 y 5" + "\n");
-        } else if (numeroUno % 3 == 0) {
-            System.out.println("El numero es divisible entre 3" + "\n");
-        } else if ( numeroUno % 5 == 0) {
-            System.out.println("El numero es divisible entre 5" + "\n");
-        }else {
-            System.out.println("El numero no es divisible entre ninguno" + "\n");
+        //COMPARAR PRIMERA MITAD
+        while (a < mitadUno.length()) {
+            char letra = mitadUno.charAt(a);
+            if(vocales.contains(String.valueOf(letra))) {
+                contarVocalesA++;
+            }
+            a++;
+        }
+        //COMPARAR SEGUNDA MITAD
+        while (b < mitadDos.length()) {
+            char letra = mitadDos.charAt(b);
+            if(vocales.contains(String.valueOf(letra))) {
+                contarVocalesB++;
+            }
+            b++;
         }
 
+        boolean sonIguales = contarVocalesA == contarVocalesB;
+        System.out.println("Las mitades contienen la misma cantidad de vocales: " + sonIguales);
 
-        //LADOS DE UN TRIANGULO
-        System.out.println("Ingresa el primer lado del traingulo: ");
-        int ladoUno = intput.nextInt();
-        System.out.println("Ingresa el segundo lado del traingulo: ");
-        int ladoDos = intput.nextInt();
-        System.out.println("Ingresa el tercer lado del traingulo: ");
-        int ladoTres = intput.nextInt();
 
-        boolean isoceles = ((ladoUno == ladoDos || ladoUno == ladoTres || ladoDos == ladoTres) && ( ladoUno != ladoTres || ladoDos != ladoTres));
-        boolean equilatero = (ladoUno == ladoDos || ladoUno == ladoTres || ladoDos == ladoTres);
 
-        if (isoceles) {
-            System.out.println("El triangulo es isoceles"+ "\n");
-        } else if (equilatero) {
-            System.out.println("El triangulo es equilatero"+ "\n");
-        } else {
-            System.out.println("El triangulo es escaleno"+ "\n");
-        }
 
-        //SALARIO DE UN CLIENTE
-        System.out.println("Ingresa tu salario: " + "\n");
-        int salario = intput.nextInt();
 
-        if (salario <= 10000){
-            System.out.println("No pagas impuestos");
-        } else if (salario >= 10001 && salario <= 20000) {
-            double inpuestoUno = salario * 0.10;
-            System.out.println("Pagaras impuesto del 10%, es decir :" + "$" +inpuestoUno + "\n");
-        } else if (salario >= 20000) {
-            double inpuestoDos = salario * 0.20;
-            System.out.println("Pagaras impuesto del 20%, es decir: " + "$" + inpuestoDos + "\n");
-        }
 
-       //PAR O IMPAR
-        System.out.println("Ingresa otro numero: ");
-        int numero2 = intput.nextInt();
 
-        if (numero2 % 2 == 0){
-            System.out.println("Es par");
-        }else {
-            System.out.println("Es impar");
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
